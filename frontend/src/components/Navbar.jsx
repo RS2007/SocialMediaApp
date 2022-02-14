22ec4562dddc9793d5bfd62e90ebfde6d9ba689d
Rohith <rohithsuresh207@gmail.com>
2022-02-14 07:49:35 +0530

    More and more stuff

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,52 +1,57 @@
 import React from "react";
-import { Flex, Heading, Icon, HStack } from "@chakra-ui/react";
+import {Flex, Heading, Icon, HStack, useMediaQuery} from "@chakra-ui/react";
 import "../pages/login.css";
 import SearchBar from "./SearchBar";
-import { FaHome } from "react-icons/fa";
-import { BiMessageRoundedDetail } from "react-icons/bi";
+import {FaHome} from "react-icons/fa";
+import {BiMessageRoundedDetail} from "react-icons/bi";
 import {
   AiOutlineCompass,
   AiOutlineHeart,
   AiOutlinePlusSquare,
 } from "react-icons/ai";
-import { HiOutlineUserCircle } from "react-icons/hi";
+import {HiOutlineUserCircle} from "react-icons/hi";
 
 const Navbar = () => {
+  const [searchBarDisplay] = useMediaQuery("(min-width:741px)");
   return (
     <Flex justify="center" w="100%" h="60px" borderBottom="1px solid #efefef">
       <Flex w="100%" maxW="950px" h="60px" justify="space-between">
-        <Flex w="20%" h="60px" colSpan={6} align="center" justify="center">
+        <Flex w={{base: "50%", md: "20%"}} h="60px" justify="center" padding="0">
           <Heading
+            as="h3"
             style={{
               fontFamily: "'Pacifico'",
               height: "45px",
               fontSize: "25px",
+              margin: "0",
+              position: "absolute",
+              bottom: "0",
             }}
           >
             Instaclone
           </Heading>
         </Flex>
-        <Flex w="25%" h="60px" align="center" justify="100%" colSpan={8}>
+        {searchBarDisplay && (<Flex w="25%" h="60px" align="center" justify="100%" colSpan={8}>
           <SearchBar />
-        </Flex>
+        </Flex>)}
 
-        <HStack w="35%">
-          <Flex w="10%" justify="center" align="center">
+        <HStack w={searchBarDisplay ? "35%" : "60%"}>
+          <Flex w="20%" maxW="40px" justify="center" align="center">
             <Icon as={FaHome} h="100%" w="80%" />
           </Flex>
-          <Flex w="10%" justify="center" align="center">
+          <Flex w="20%" maxW="40px" justify="center" align="center">
             <Icon as={BiMessageRoundedDetail} h="100%" w="80%" />
           </Flex>
-          <Flex w="10%" justify="center" align="center">
+          <Flex w="20%" maxW="40px" justify="center" align="center">
             <Icon as={AiOutlinePlusSquare} h="100%" w="80%" />
           </Flex>
-          <Flex w="10%" justify="center" align="center">
+          <Flex w="20%" maxW="40px" justify="center" align="center">
             <Icon as={AiOutlineCompass} h="100%" w="80%" />
           </Flex>
-          <Flex w="10%" justify="center" align="center">
+          <Flex w="20%" maxW="40px" justify="center" align="center">
             <Icon as={AiOutlineHeart} h="100%" w="80%" />
           </Flex>
-          <Flex w="10%" align="center" justify="center">
+          <Flex w="20%" maxW="40px" align="center" justify="center">
             <Icon as={HiOutlineUserCircle} h="100%" w="80%" />
           </Flex>
         </HStack>
--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -1,13 +1,17 @@
-import { React } from "react";
-import { Input, Icon, InputGroup, InputLeftElement } from "@chakra-ui/react";
-import { FaSearch } from "react-icons/fa";
+import {React, useState} from "react";
+import {Input, Icon, InputGroup, InputLeftElement} from "@chakra-ui/react";
+import {FaSearch} from "react-icons/fa";
 const SearchBar = () => {
+  const [searchInput, setSearchInput] = useState("")
+  const handleSearchInputChange = (e) => {
+    setSearchInput(e.target.value)
+  }
   return (
     <InputGroup>
       <InputLeftElement>
         <Icon as={FaSearch} color="gray.300" />
       </InputLeftElement>
-      <Input w="100%" placeholder="Search" bg="#efefef" />
+      <Input w="100%" placeholder="Search" bg="#efefef" value={searchInput} onClick={handleSearchInputChange} />
     </InputGroup>
   );
 };
